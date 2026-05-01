bec0d23a0c5fb258b77e016d9f2075734fd97a0a
BCXOVER <dmitri7black7clover.com@gmail.com>
2026-05-01 03:38:28 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,63 +1,9 @@
-const CACHE = "justclover-anime-rave-v1";
-
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./firebase-config.js",
-  "./manifest.webmanifest",
-  "./icons/icon.svg"
-];
-
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-
+self.addEventListener('install', event => self.skipWaiting());
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(CACHE).then((cache) => {
-      return cache.addAll(ASSETS).catch(() => {});
-    })
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys
-          .filter((key) => key !== CACHE)
-          .map((key) => caches.delete(key))
-      );
-    }).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  const url = new URL(event.request.url);
-
-  if (
-    url.hostname.includes("firebaseio.com") ||
-    url.hostname.includes("googleapis.com") ||
-    url.hostname.includes("gstatic.com") ||
-    url.hostname.includes("youtube.com") ||
-    url.hostname.includes("ytimg.com") ||
-    url.hostname.includes("vk.com") ||
-    url.hostname.includes("anilibrix")
-  ) {
-    return;
-  }
-
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const copy = response.clone();
-
-        caches.open(CACHE).then((cache) => {
-          cache.put(event.request, copy).catch(() => {});
-        });
-
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
-});
+self.addEventListener('fetch', () => {});
