6224a193e2601ecdee1f1014bf5ef6e2ce9048e6
BCXOVER <dmitri7black7clover.com@gmail.com>
2026-05-02 16:54:11 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* JustClover cache killer stage37-catalog-hard-scroll-fix-20260502-1 */
+/* JustClover cache killer stage38-clean-scroll-focus-rewrite-20260502-1 */
 self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
@@ -7,8 +7,8 @@ self.addEventListener('activate', event => {
   event.waitUntil((async () => {
     const keys = await caches.keys();
     await Promise.all(keys.map(k => caches.delete(k)));
+    if (self.registration && self.registration.unregister) await self.registration.unregister();
     await self.clients.claim();
-    try { await self.registration.unregister(); } catch(e) {}
   })());
 });
 self.addEventListener('fetch', event => {
