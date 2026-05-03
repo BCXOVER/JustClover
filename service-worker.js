2c99a0d6105c9239a8583c2378bac5c41b6b49d1
BCXOVER <dmitri7black7clover.com@gmail.com>
2026-05-03 23:03:05 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,7 @@
-const JC_CACHE_BUSTER = 'stage123-rave-anime-room-customizer-20260503-1';
-self.addEventListener('install', event => { self.skipWaiting(); });
-self.addEventListener('activate', event => {
-  event.waitUntil((async () => {
-    const keys = await caches.keys();
-    await Promise.all(keys.map(key => caches.delete(key)));
-    await self.clients.claim();
-  })());
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(self.skipWaiting());
+});
+self.addEventListener('activate', function(event) {
+    event.waitUntil(self.clients.claim());
 });
